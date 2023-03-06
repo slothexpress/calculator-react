d1be7701b25d1d39183a08e797a3e58ead48c199
slothexpress <hellosami@protonmail.com>
2023-03-06 16:44:22 +0100

    Replace buttons with DigitButton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { useReducer } from "react";
+import DigitButton from "./DigitButton";
 import "./styles.css";
 
-const ACTIONS = {
+export const ACTIONS = {
   ADD_DIGIT: 'add-digit',
   CHOOSE_OPERATION: 'choose-operation',
   CLEAR: 'clear',
@@ -23,8 +24,6 @@ function reducer(state, { type, payload }) {
 function App() {
   const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {});
   
-  dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 }});
-
   return (
     <div className="calculator-grid">
       <div className="output">
@@ -46,23 +45,27 @@ function App() {
         /
       </button>
 
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
+      <DigitButton dispatch={dispatch} digit="1" />
+      <DigitButton dispatch={dispatch} digit="2" />
+      <DigitButton dispatch={dispatch} digit="3" />
       <button>*</button>
-
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
+      <DigitButton dispatch={dispatch} digit="4" />
+      <DigitButton dispatch={dispatch} digit="5" />
+      <DigitButton dispatch={dispatch} digit="6" />
       <button>+</button>
-
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
+      <DigitButton dispatch={dispatch} digit="7" />
+      <DigitButton dispatch={dispatch} digit="8" />
+      <DigitButton dispatch={dispatch} digit="9" />
       <button>-</button>
-
       <button>.</button>
-      <button>0</button>
+      <DigitButton dispatch={dispatch} digit="0" />
+
+
+
+
+
+
+      
       <button className="span-two">
         =
       </button>
